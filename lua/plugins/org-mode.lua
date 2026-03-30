d4d44984ae224001db7e418c61bfa2c5e3b8c02d
AlexandreFontana <alexandrefontana02@gmail.com>
2026-03-30 14:36:42 +0800

    added orgmode working

--- lua/plugins/org-mode.lua
+++ lua/plugins/org-mode.lua
@@ -2,20 +2,23 @@ return {
   {
     'nvim-orgmode/orgmode',
     event = 'VeryLazy',
+    ft = { 'org' },
     config = function()
       -- Setup orgmode
       require('orgmode').setup({
-        org_agenda_files = '~/Documents/vault/*',
-        org_default_notes_file = '~/Documents/vault/*',
+        org_agenda_files = '~/vault/*',
+        org_default_notes_file = '~/vault/*',
       })
       -- Experimental LSP support
       vim.lsp.enable('org')
     end,
+    
   },
 
   {
-    "nvim-treesitter/nvim-treesitter",
-    build = ":TSUpdate",
-  }
+    'nvim-treesitter/nvim-treesitter',
+    lazy = false,
+    build = ':TSUpdate',
+  },
 
   }
